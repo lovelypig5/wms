8c6f7759b820cb8f406fda1c312b3a84ac6a6e3a
wei.ding <wei.ding@datayes.com>
2016-09-06 10:35:47 +0800

    fix bug

--- assets/js/common/pagination.vue
+++ assets/js/common/pagination.vue
@@ -1,13 +1,13 @@
 <template>
     <nav class="center">
         <ul class="pagination" v-show="pagination.count > 1">
-            <li :class="{disabled: pagination.page == 1}" @click="back10">
+            <li :class="{disabled: pagination.page <= 1}" @click="back10">
                 <a href="javascript:void(0)" aria-label="Previous">
                     <span aria-hidden="true">&laquo;</span>
                 </a>
             </li>
-            <li v-for="i in pagination.count" :class="{active: pagination.page == i + 1}" @click="change(i+1)"><a href="javascript:void(0)">{{ i + 1 }}</a></li>
-            <li :class="{disabled: pagination.page == pagination.count}" @click="forword10">
+            <li v-for="i in pagination.count" v-if="i >= pagination.page - 1 && i < pagination.page + 9" :class="{active: pagination.page == i + 1}" @click="change(i+1)"><a href="javascript:void(0)">{{ i + 1 }}</a></li>
+            <li :class="{disabled: pagination.page >= pagination.count}" @click="forword10">
                 <a href="javascript:void(0)" aria-label="Next">
                     <span aria-hidden="true">&raquo;</span>
                 </a>
@@ -20,10 +20,16 @@ var Pagination = Vue.extend({
     props: ['pagination', 'change'],
     methods: {
         back10() {
+            if (this.pagination.page <= 1) {
+                return
+            }
             var page = this.pagination.page - 10 < 1 ? 1 : this.pagination.page - 10;
             this.change(page);
         },
         forword10() {
+            if (this.pagination.page >= this.pagination.count) {
+                return
+            }
             var page = this.pagination.page + 10 > this.pagination.count ? this.pagination.count : this.pagination.page + 10;
             this.change(page);
         }
