9f8156f4ad53e82d199d9c49fcb0ff83b050fbed
lovelypig5 <lovelypig5@163.com>
2016-11-02 20:35:40 +0800

    new templates

--- assets/js/features/order/list.vue
+++ assets/js/features/order/list.vue
@@ -1,66 +1,11 @@
-<template>
-<div class="panel panel-primary good-list">
-    <div class="panel-heading">订单列表</div>
-    <table class="table" v-if="!loading.fetch && orderList.length > 0">
-        <thead>
-            <tr>
-                <th>订单号</th>
-                <th>快递单号</th>
-                <th>快递费用</th>
-                <th>收件人</th>
-                <th>价格</th>
-                <th>操作</th>
-                <th>发货时间</th>
-            </tr>
-        </thead>
-        <tbody>
-            <template v-for="order in orderList">
-                    <tr class="item" >
-                        <td>
-                            {{order.id}}
-                        </td>
-                        <td>{{order.expressId}}</td>
-                        <td>
-                            {{order.expressCost}}
-                        </td>
-                        <td>
-                            {{order.name}}
-                        </td>
-                        <td>
-                            {{order.price}}
-                        </td>
-                        <td><a href="javascript:void(0)" @click="expand(order)" v-if="!order.goodList">展开</a>
-                            <div v-if="order.goodList && order.goodList.length > 0" v-for="good in order.goodList">
-                                <span>{{ good.name }}</span>
-                                <span>({{ good.attrs | join-attrs }})</span>
-                                <span>*{{ good.amount }}</span>
-                            </div>
-                        </td>
-                        <td>
-                            {{order.date}}
-                        </td>
-                    </tr>
-                    <tr v-if="order.comment">
-                        <td colspan="7">{{order.comment}}</td>
-                    </tr>
-</template>
-            </tbody>
-        </table>
-        <pagination :pagination="params" :change="change"></pagination>
-        <div class="empty" v-if="!loading.fetch && orderList.length == 0">
-            <div class="msg">商品列表为空</div>
-        </div>
-        <div v-if="loading.fetch">
-            <div class="loading audio-wave"></div>
-        </div>
-    </div>
-</template>
 <script>
+import template from 'templates/order/list.html';
 import API from '../../config/api';
 import actions from '../../vuex/actions';
 import Pagination from '../../common/pagination.vue';
 
 var OrderList = Vue.extend({
+    template: template,
     name: 'orderList',
     components: {
         pagination: Pagination
